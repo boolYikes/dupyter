5923d7628b61dafb9a8b2112b40e2dab464e383b
Dee Henry Seon <tunacome@gmail.com>
2025-04-10 04:50:51 +0000

    Repurpose repository as multi-util repo

---
+++ README.md
@@ -0,0 +1,7 @@
+**Some convinience containers for cleaner experimentations**
+*I AM NOT OCD*
+- Jupyter container
+    - To be used as a Jupyter server & simple script executor
+- Python slim container
+    - Can pass docker network, script, req as arguments
+    - Takes a load off my experiments...
---
+++ python/rc.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+NETWORK=$1
+SCRIPT_PATH=$2
+REQUIREMENTS_FILE=$3
+
+if [ -z "$SCRIPT_PATH" ]; then
+    echo "Usage: $0 <docker-network> <local-python-script-path> <requirements-path>"
+    echo "You at least must provide python script path"
+    exit 1
+fi
+
+# Use default if no network was provided
+if [ -z "$NETWORK" ]; then
+    NETWORK="bridge"
+fi
+# Use default name if no req was provided
+if [ -z "$REQUIREMENTS_FILE" ]; then
+    REQUIREMENTS_FILE="requirements.txt"
+fi
+
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+SCRIPT_FILE=$(basename "$SCRIPT_PATH")
+
+# Make an empty req file as default
+if [ -f "$SCRIPT_DIR/$REQUIREMENTS_FILE" ]; then
+    touch "$SCRIPT_DIR/$REQUIREMENTS_FILE"
+fi
+
+docker run --rm -it \
+    --network "$NETWORK" \
+    -v "$SCRIPT_DIR":/app \
+    -w /app \
+    python:3.14-rc-slim \
+    bash -c "pip install -r '$REQUIREMENTS_FILE' --quiet && python '$SCRIPT_FILE'"
